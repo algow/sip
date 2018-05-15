5a8fde8b933bae8303f82f8c656eff2b5a61f414
algow <algow64@gmail.com>
2018-05-15 23:41:44 +0700

    Fix bug JSON untuk FO

--- resources/views/datatable/_aksi.blade.php
+++ resources/views/datatable/_aksi.blade.php
@@ -18,45 +18,46 @@
     @endisset
     @empty($tanggal)
         <button type="button" class="btn-md btn-success btn-{{ $id }}"><i class="fa fa-play fa-md"></i></button>
-        <form action = "{{ route('spm.diambil') }}" method = "POST" class = "form-hide-{{ $id }}" onSubmit="return setPetugas(event);">
+        <p id="paragraf-{{ $id }}" class="bg-primary"></p>
+        <form action = "{{ route('spm.diambil') }}" method = "POST" id = "form-hide-{{ $id }}">
            {{ csrf_field() }}
            {{ Form::hidden('id', $id) }}
-           {{ Form::text('pengambil', null, ['class'=>'form-control input-sm']) }}
+           {{ Form::text('pengambil', null, ['class'=>'form-control input-sm', 'placeholder'=>'Scan disini']) }}
         </form>
     @endempty
 @endif
 
 <script>
     $(document).ready(function(){
-        $(".form-hide-{{ $id }}").hide();
+        $("#form-hide-{{ $id }}").hide();
         $(".btn-{{ $id }}").click(function(){
-            $(".form-hide-{{ $id }}").fadeIn("slow");
+            $("#form-hide-{{ $id }}").fadeIn("slow");
             $(".btn-{{ $id }}").hide("slow");
         });
+
         $(document.body).on('submit', '.konfirmasi', function () {
             var $el = $(this);
             var text = $el.data('confirm') ? $el.data('confirm') : 'Yakin akan menghapus record ini?';
             var c = confirm(text);
             return c;
         });
-    });
 
-    function setPetugas(e)
-    {
-        var _token = $("input[name='_token']").val();
-        var id = $("input[name='id']").val();
-        var pengambil = $("input[name='pengambil']").val();
+        $("#form-hide-{{ $id }}").submit(function(event) {
+            var _token = $('#form-hide-{{ $id }}').find('input[name="_token"]').val();
+            var id = $('#form-hide-{{ $id }}').find('input[name="id"]').val();
+            var pengambil = $('#form-hide-{{ $id }}').find('input[name="pengambil"]').val();
 
-        $.ajax({
-            dataType:"json",
-            type:"POST",
-            url: "{{ route('spm.diambil') }}",
-            data: {"_token" : _token, "id" : id, "pengambil" : pengambil},
-            success: function(data) {
-              alert('Keterangan: ' + data[1]);
-            }
+            $.ajax({
+                dataType:"json",
+                type:"POST",
+                url: "{{ route('spm.diambil') }}",
+                data: {"_token" : _token, "id" : id, "pengambil" : pengambil},
+                success: function(data) {
+                  $("#form-hide-{{ $id }}").hide("slow");
+                  $("#paragraf-{{ $id }}").append(data[1]);
+                }
+            });
+            event.preventDefault();
         });
-        e.preventDefault();
-        return false;
-    }
+    });
 </script>
