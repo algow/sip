815ea1c618df36c861c3039d7ced243578b5ed40
Algo Wijaya <34807887+algow@users.noreply.github.com>
2018-05-21 10:14:49 +0700

    Update QueryController.php

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -21,43 +21,7 @@ class QueryController extends Controller
         $this->jenis = $request->input('jenis');
         $this->tanggalTerima = date('d F Y', strtotime($this->tanggal));
         $this->query();
-
-/*        if(empty($this->jenis))
-        {
-            if (isset($this->query_satker) && empty($this->query_tanggal)) {
-                $this->query = Spm::with('petugas')->where('kode_satker', $this->query_satker)->get();
-            }
-            elseif (isset($this->query_tanggal) && empty($this->query_satker)) {
-                $this->query = Spm::with('petugas')->where('tanggal_terima', $this->query_tanggal)->get();
-            }
-            elseif (isset($this->query_satker) && isset($this->query_tanggal)) {
-                $this->query = Spm::with('petugas')->where('kode_satker', $this->query_satker)
-                        ->where('tanggal_terima', $this->query_tanggal)->get();
-            }
-            else {
-                $this->query = Spm::with('petugas')->get();
-            }
-        }
-        else
-        {
-            if (isset($this->query_satker) && empty($this->query_tanggal)) {
-                $this->query = Spm::with('petugas')->where('kode_satker', $this->query_satker)
-                        ->where('jenis', $this->jenis)->get();
-            }
-            elseif (isset($this->query_tanggal) && empty($this->query_satker)) {
-                $this->query = Spm::with('petugas')->where('tanggal_terima', $this->query_tanggal)
-                        ->where('jenis', $this->jenis)->get();
-            }
-            elseif (isset($this->query_satker) && isset($this->query_tanggal)) {
-                $this->query = Spm::with('petugas')->where('kode_satker', $this->query_satker)
-                        ->where('tanggal_terima', $this->query_tanggal)
-                        ->where('jenis', $this->jenis)->get();
-            }
-            else {
-                $this->query = Spm::with('petugas')->where('jenis', $this->jenis)->get();
-            }
-        }
-*/    }
+    }
 
     protected function query()
     {
