e0c0f989fad23acb2d223ca2c1e8a30ff07005ab
algow <algow64@gmail.com>
2018-07-14 23:05:12 +0700

    Import excel belum jadi

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Excel;
-use App\Http\Controllers\QueryController as QueryController;
+use App\Http\Controllers\QueryRead;
+use Illuminate\Support\Facades\DB;
 
 class ExcelController extends Controller
 {
-    public function query($request)
+    private $bulkQuery = [];
+    private $loop = true;
+
+    private function query($request)
     {
-        $query = new QueryController($request);
+        $query = new QueryRead($request);
 
         return $query;
     }
@@ -60,5 +64,42 @@ class ExcelController extends Controller
                 }
             });
         })->export('xlsx');
+    }
+
+    public function importSpm(Request $request)
+    {
+        $file = $request->file('import');
+
+        $excel = Excel::selectSheetsByIndex(0)->load($file, function($reader) {
+        })->get()->toArray();
+
+        $this->bulkInsert($excel);
+    }
+    private function bulkInsert($data)
+    {
+        foreach($data as $key => $value)
+        {
+            if($this->loop) {
+                if(is_array($value)) {
+                    array_push($this->bulkQuery, $value);
+                    // Rrrreecursion AWAS BUG
+                    $this->bulkInsert($value);
+                } elseif($key === 'nomor_invoice' && $this->sudahTerinput($value)) {
+                    array_pop($this->bulkQuery);
+                    $this->loop = false;
+                }
+            } else {
+                return $this->bulkQuery;
+            }
+        }
+    }
+
+    private function sudahTerinput($nomSpm)
+    {
+        // array[0] masuk id database kecil
+        // Ambil nomor spm dari query terakhir
+        // Bar ngono cocokno neng $noSpm, hasile return bool
+        $lastImport = DB::table('spms')->where('import', 1)->latest()->first();
+        return $nomSpm === $lastImport;
     }
 }
